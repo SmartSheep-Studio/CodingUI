e8fcd231588e98d24e71729f190c75c2dcd06f96
Jiang JunYang <littlesheep.code@hotmail.com>
2022-06-01 22:36:31 +0800

    release: release public test 2.0.0

--- src/views/Operations/OperationExecutePage.vue
+++ src/views/Operations/OperationExecutePage.vue
@@ -2,21 +2,233 @@
   <div class="container-size horizontal-wrapper">
     <div class="horizontal-left">
       <div class="horizontal-resize"></div>
-      <div class="editor"><MonacoEditor v-model:value="data.content" language="javascript" style="height: 100%" /></div>
+      <div class="editor">
+        <MonacoEditor v-model:value="editor.content" :language="editor.language" style="height: 100%" />
+        <div>
+          <n-grid :cols="24">
+            <n-gi :span="1" :offset="1">
+              <n-affix :bottom="20">
+                <n-tooltip placement="right">
+                  <template #trigger>
+                    <n-button strong secondary circle type="info" size="large" @click="operation.commit()">
+                      <template #icon>
+                        <n-icon>
+                          <PlayCircle />
+                        </n-icon>
+                      </template>
+                    </n-button>
+                  </template>
+                  消耗 1 能量进行模拟运行
+                </n-tooltip>
+              </n-affix>
+            </n-gi>
+          </n-grid>
+        </div>
+      </div>
     </div>
     <div class="horizontal-right">
-
+      <div style="height: calc(100vh - 64px)">
+        <n-scrollbar
+          style="height: calc(50% - 44px); width: calc(100% - 40px); padding-top: 20px; padding-left: 20px; padding-right: 20px">
+          <n-tabs v-model:value="result.focus" type="segment">
+            <n-tab-pane name="overview" tab="概览">
+              <n-empty v-if="operation.commits.length === 0" description="请先进行模拟运行" />
+              <div v-else style="padding: 20px">
+                <n-grid :cols="3">
+                  <n-gi>
+                    <n-statistic label="启动于">{{ new Date(operation.data.created_at).toLocaleString() }}</n-statistic>
+                  </n-gi>
+                  <n-gi>
+                    <n-statistic label="活动于">{{ new Date(operation.data.updated_at).toLocaleString() }}</n-statistic>
+                  </n-gi>
+                  <n-gi>
+                    <n-statistic label="提交数" :value="operation.commits.length"></n-statistic>
+                  </n-gi>
+                </n-grid>
+              </div>
+            </n-tab-pane>
+            <n-tab-pane name="commits" tab="提交明细">
+              <n-empty v-if="operation.commits.length === 0" description="请先进行模拟运行" />
+              <n-card v-else>
+                <n-collapse accordion>
+                  <n-collapse-item v-for="(commit, index) in operation.commits" :title="'提交 #' + (index + 1)"
+                    :name="'commit' + commit.id" :key="index">
+                    <n-space vertical>
+                      <n-card hoverable size="small" title="提交代码">
+                        <n-code :code="commit.code"></n-code>
+                      </n-card>
+                      <n-card hoverable size="small" title="提交状态">
+                        <n-checkbox v-for="(mark, index) in commit.data['marks']" :key="index" :checked="mark">用例
+                          #{{ index + 1 }}</n-checkbox>
+                      </n-card>
+                    </n-space>
+                  </n-collapse-item>
+                </n-collapse>
+              </n-card>
+            </n-tab-pane>
+          </n-tabs>
+        </n-scrollbar>
+        <n-divider />
+        <n-scrollbar
+          style="height: calc(50% - 25px); width: calc(100% - 40px); padding-left: 20px; padding-right: 20px">
+          <n-space :size="24" vertical>
+            <n-thing title="剧情">
+              <div v-html="operation.operation['story']"></div>
+            </n-thing>
+            <n-thing v-if="operation.operation['description'] !== ''" title="简介">
+              <div v-html="operation.operation['description']"></div>
+            </n-thing>
+            <n-thing title="消耗">
+              <n-list bordered>
+                <n-list-item v-for="(cost, index) in operation.operation['costs']" :key="index">
+                  <span v-if="cost['id'] === 'rational'">理智</span>
+                  <template #suffix>{{ cost["amount"] }}</template>
+                </n-list-item>
+              </n-list>
+            </n-thing>
+            <n-thing title="奖励">
+              <n-list bordered>
+                <n-list-item v-for="(reward, index) in operation.operation['rewards']" :key="index">
+                  <span v-if="reward['id'] === 'code-coin'">逻辑币</span>
+                  <template #suffix>{{ reward["amount"] }}</template>
+                </n-list-item>
+              </n-list>
+            </n-thing>
+            <n-thing title="用例">
+              <n-card>
+                <n-collapse>
+                  <n-collapse-item v-for="(item, index) in operation.operation['judgement']" :key="index"
+                    :name="'example' + index" :title="'样例 #' + (index + 1)" size="small">
+                    <n-space vertical>
+                      <n-card hoverable size="small" title="输入数据">
+                        <n-code :code="item['stdin']"></n-code>
+                      </n-card>
+                      <n-card hoverable size="small" title="输出数据">
+                        <n-code :code="item['stdout']"></n-code>
+                      </n-card>
+                    </n-space>
+                  </n-collapse-item>
+                </n-collapse>
+              </n-card>
+            </n-thing>
+          </n-space>
+        </n-scrollbar>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { NTabs, NEmpty, NTabPane, NCollapse, NCollapseItem, NCard, NList, NListItem, NThing, NSpace, NScrollbar, NDivider, NCode, NAffix, NGrid, NGi, NIcon, NButton, NStatistic, NCheckbox, NTooltip, useMessage } from "naive-ui";
+import { PlayCircle } from "@vicons/ionicons5";
+import { inject, onMounted, reactive, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { VueCookies } from "vue-cookies";
+import { Axios } from "axios";
+import { useStatusStore } from "../../stores/status";
 import MonacoEditor from "../../compoments/MonacoEditor.vue";
-import {reactive} from "vue";
 
-const data = reactive(({
-  content: "console.log('Hello World')"
-}))
+const cookies = inject("$cookies") as VueCookies;
+const store = useStatusStore();
+const message = useMessage();
+const axios = inject("axios") as Axios;
+const router = useRouter();
+const route = useRoute();
+const editor = reactive({
+  content: "print('Hello World')",
+  language: "python",
+  runtime: {
+    name: "python3",
+    version: "3.9.4",
+  }
+});
+const result = reactive({
+  focus: "overview",
+});
+
+const operation: any = reactive({
+  id: route.query["id"],
+  data: {},
+  operation: {},
+  commits: [],
+  fetch: async () => {
+    let response;
+    // Fetch Log
+    response = await axios.get("/api/operations/log?id=" + operation.id, {
+      headers: { Authorization: "Bearer " + cookies.get("access_token") },
+    });
+    operation.data = response.data["Response"];
+    if (operation.data["status"] !== "working") {
+      message.error("无法获取行动数据或行动已经完成，请从订单列表选择目标行动来重新开始行动");
+      await router.push({ name: "Operation.Orders" });
+      return;
+    }
+    // Fetch Operation
+    response = await axios.get("/api/operations/detail?id=" + operation.data["operation"], {
+      headers: { Authorization: "Bearer " + cookies.get("access_token") },
+    });
+    operation.operation = response.data["Response"];
+    if (response.data["Response"] == null) {
+      message.error("无法获取行动记录的行动，可能行动已经失效，请从订单列表选择目标行动来重新开始行动");
+      await router.push({ name: "Operation.Orders" });
+      return;
+    } else {
+      operation.operation["story"] = operation.operation["story"].replace(
+        "{DOCTOR_NAME}",
+        store.profile.user["username"]
+      );
+      operation.operation["description"] = operation.operation["description"].replace(
+        "{DOCTOR_NAME}",
+        store.profile.user["username"]
+      );
+    }
+    // Fetch Commits
+    response = await axios.get("/api/operations/commits?id=" + operation.id, {
+      headers: { Authorization: "Bearer " + cookies.get("access_token") },
+    });
+    operation.commits = response.data["Response"]
+  },
+
+  commit: async () => {
+    const response = await axios.post("/api/operations/commit?runtime=" + editor.runtime.name + "&runtimeVersion=" + editor.runtime.version, {
+      id: operation.id,
+      code: editor.content,
+    }, {
+      headers: { Authorization: "Bearer " + cookies.get("access_token") },
+    });
+
+    if (response.status === 400) {
+      message.error("提交代码失败，请检查您的代码何选择的运行环境是否有效")
+    } else if (response.status === 402) {
+      message.warning("提交代码失败，需要的资源不足，请检查仓库")
+    } else if (response.status === 200) {
+      if (response.data["Status"]["Code"] === "FINISHED") {
+        message.success("成功提交代码，所有用例通过，行动完成")
+        await router.push({ name: "Operation.Orders" })
+        return
+      }
+      message.success("成功提交代码，但模拟用例未通过，请重新检查")
+      operation.commits.push(response.data["Response"])
+    }
+  },
+});
+
+watch(
+  store.profile,
+  async () => {
+    await operation.fetch();
+  },
+  { deep: true }
+);
+
+onMounted(async () => {
+  if (route.query["id"] == null) {
+    message.error("无法获取行动记录 ID，请从订单列表选择目标行动来开始行动");
+    router.push({ name: "Operation.Orders" });
+  } else {
+    await operation.fetch();
+  }
+});
 </script>
 
 <style>
@@ -32,22 +244,20 @@ const data = reactive(({
   min-width: 30vw;
   width: 40vw;
   max-width: 60vw;
-  background: #ccc;
   resize: horizontal;
   overflow: scroll;
   height: calc(100vh - 64px);
-  opacity: 0;
+  opacity: 1;
   position: relative;
 }
 
 .horizontal-resize::-webkit-scrollbar {
-  width: 20px;
+  width: 10px;
   height: inherit;
 }
 
 .horizontal-right {
-  background: #f0f;
-  flex: 1
+  flex: 1;
 }
 
 .editor {
--- src/views/Operations/OperationListPage.vue
+++ src/views/Operations/OperationListPage.vue
@@ -8,43 +8,33 @@
               <n-checkbox v-model:checked="filter.working">仅行动中</n-checkbox>
             </n-space>
             <div>
-              <n-button :loading="connecting" size="small" type="primary" @click="operations.fetch()"
-              >重新获取
-              </n-button
-              >
+              <n-button :loading="connecting" size="small" type="primary" @click="operations.fetch()">重新获取
+              </n-button>
             </div>
           </n-space>
           <n-list bordered>
             <n-list-item v-if="operations.logs.length === 0">
-              <n-empty description="目前没有任何行动记录"/>
+              <n-empty description="目前没有任何行动记录" />
             </n-list-item>
             <n-list-item v-for="(item, index) in operations.logs.slice(
-                (operations.pagination.logs - 1) * 3,
-                (operations.pagination.logs - 1) * 3 + 3
-              )" v-else :key="index">
-              <n-thing
-                  :title="item['operation']"
-                  :title-extra="new Date(item['created_at']).toLocaleString()"
-              >
-                <template #description
-                >
-                  <n-space justify="space-between"
-                  ><span>
-                    <n-tag v-if="item['status'] === 'finished'" type="success">行动成功</n-tag>
-                    <n-tag
-                        v-else-if="item['status'] === 'canceled'"
-                        type="error">行动取消</n-tag>
-                    <n-tag
-                        v-else type="info">行动中</n-tag>
-                  </span><span>#{{ item['id'] }}</span></n-space
-                  >
-                </template
-                >
+              (operations.pagination.logs - 1) * 3,
+              (operations.pagination.logs - 1) * 3 + 3
+            )" v-else :key="index">
+              <n-thing :title="item['operation']" :title-extra="new Date(item['created_at']).toLocaleString()"
+                @click="() => { if (item['status'] === 'working') { $router.push({ name: 'Operation.Execute', query: { id: item['id'] } }) } }">
+                <template #description>
+                  <n-space justify="space-between"><span>
+                      <n-tag v-if="item['status'] === 'finished'" type="success">行动成功</n-tag>
+                      <n-tag v-else-if="item['status'] === 'canceled'" type="error">行动取消</n-tag>
+                      <n-tag v-else type="info">行动可用</n-tag>
+                    </span><span>#{{ item['id'] }}</span></n-space>
+                </template>
               </n-thing>
             </n-list-item>
           </n-list>
           <n-space justify="center">
-            <n-pagination v-model:page="operations.pagination.logs" :page-count="Math.ceil(operations.logs.length / 3)"/>
+            <n-pagination v-model:page="operations.pagination.logs"
+              :page-count="Math.ceil(operations.logs.length / 3)" />
           </n-space>
         </n-card>
       </n-grid-item>
@@ -56,44 +46,31 @@
               <n-checkbox v-model:checked="filter.unfinished">仅未完成</n-checkbox>
             </n-space>
             <div>
-              <n-button :loading="connecting" size="small" type="primary" @click="operations.fetch()"
-              >重新获取
-              </n-button
-              >
+              <n-button :loading="connecting" size="small" type="primary" @click="operations.fetch()">重新获取
+              </n-button>
             </div>
           </n-space>
           <n-list bordered>
             <n-list-item v-if="operations.data.length === 0">
-              <n-empty description="目前没有任何订单"/>
+              <n-empty description="目前没有任何订单" />
             </n-list-item>
             <n-list-item v-for="(item, index) in operations.data.slice(
-                (operations.pagination.operations - 1) * 10,
-                (operations.pagination.operations - 1) * 10 + 10
-              )" v-else :key="index">
-              <n-thing
-                  :title="item['title']"
-                  :title-extra="item['category']"
-                  @click="preview.preview(item)"
-              >
-                <template #description
-                >
-                  <n-space justify="space-between"
-                  ><span
-                  ><span
-                  >需求等级 <b>Lv{{ item["conditions"]["level"] }}</b></span
-                  >&nbsp;
-                      <span
-                      >需求关卡 <b>{{ item["conditions"]["progress"] }}</b></span
-                      ></span
-                  ><span>{{ item["id"] }}</span></n-space
-                  >
-                </template
-                >
+              (operations.pagination.operations - 1) * 10,
+              (operations.pagination.operations - 1) * 10 + 10
+            )" v-else :key="index">
+              <n-thing :title="item['title']" :title-extra="item['category']" @click="preview.preview(item)">
+                <template #description>
+                  <n-space justify="space-between"><span><span>需求等级 <b>Lv{{ item["conditions"]["level"]
+                  }}</b></span>&nbsp;
+                      <span>需求关卡 <b>{{ item["conditions"]["progress"] }}</b></span></span><span>{{ item["id"] }}</span>
+                  </n-space>
+                </template>
               </n-thing>
             </n-list-item>
           </n-list>
           <n-space justify="center">
-            <n-pagination v-model:page="operations.pagination.operations" :page-count="Math.ceil(operations.data.length / 10)"/>
+            <n-pagination v-model:page="operations.pagination.operations"
+              :page-count="Math.ceil(operations.data.length / 10)" />
           </n-space>
         </n-card>
       </n-grid-item>
@@ -126,32 +103,23 @@
           <n-thing title="用例">
             <n-card>
               <n-collapse>
-                <n-collapse-item
-                    v-for="(item, index) in preview.previewing['judgement']"
-                    :key="index"
-                    :name="'example' + index"
-                    :title="'样例 #' + (index + 1)"
-                    size="small"
-                >
-                  <n-space vertical
-                  >
+                <n-collapse-item v-for="(item, index) in preview.previewing['judgement']" :key="index"
+                  :name="'example' + index" :title="'样例 #' + (index + 1)" size="small">
+                  <n-space vertical>
                     <n-card hoverable size="small" title="输入数据">
                       <n-code :code="item['stdin']"></n-code>
-                    </n-card
-                    >
-                    <n-card hoverable size="small" title="输出数据"
-                    >
+                    </n-card>
+                    <n-card hoverable size="small" title="输出数据">
                       <n-code :code="item['stdout']"></n-code>
                     </n-card>
-                  </n-space
-                  >
+                  </n-space>
                 </n-collapse-item>
               </n-collapse>
             </n-card>
           </n-thing>
         </n-space>
         <template #footer>
-          <n-button size="small" type="primary">开始行动</n-button>
+          <n-button size="small" type="primary" @click="preview.start()" :loading="connecting">开始行动</n-button>
         </template>
       </n-drawer-content>
     </n-drawer>
@@ -159,7 +127,7 @@
 </template>
 
 <script lang="ts" setup>
-import {Axios} from "axios";
+import { Axios } from "axios";
 import {
   NGrid,
   NGridItem,
@@ -180,30 +148,47 @@ import {
   NPagination,
   useMessage,
 } from "naive-ui";
-import {inject, reactive, ref, watch} from "vue";
-import {VueCookies} from "vue-cookies";
-import {useStatusStore} from "../../stores/status";
+import { inject, reactive, ref, watch } from "vue";
+import { VueCookies } from "vue-cookies";
+import { useRouter } from "vue-router";
+import { useStatusStore } from "../../stores/status";
 
 const cookies = inject("$cookies") as VueCookies;
 const message = useMessage();
 const store = useStatusStore();
+const router = useRouter();
 const axios = inject("axios") as Axios;
 const connecting = ref(false);
 const preview: any = reactive({
   display: false,
   previewing: {},
-  preview: (item: object) => {
+  preview: (item: any) => {
     preview.display = true;
     preview.previewing = item;
     preview.previewing["story"] = preview.previewing["story"].replace(
-        "{DOCTOR_NAME}",
-        store.profile.user["username"]
+      "{DOCTOR_NAME}",
+      store.profile.user["username"]
     );
     preview.previewing["description"] = preview.previewing["description"].replace(
-        "{DOCTOR_NAME}",
-        store.profile.user["username"]
+      "{DOCTOR_NAME}",
+      store.profile.user["username"]
     );
   },
+  start: async () => {
+    connecting.value = true;
+    const response = await axios.post("/api/operations/start", { id: preview.previewing["id"] }, {
+      headers: { Authorization: "Bearer " + cookies.get("access_token") },
+    });
+    if (response.status === 402) {
+      message.warning("无法开展行动，资源或者行动槽位不足，请检查行动消耗")
+    } else if (response.status === 402) {
+      message.error("无法开展行动，找不到目标行动或者行动已终止，请刷新行动列表")
+    } else {
+      message.success("行动成功开展")
+      router.push({ name: "Operation.Execute", query: { id: response.data["Response"]["id"] } })
+    }
+    connecting.value = false;
+  }
 });
 const filter = reactive({
   ignore: false,
@@ -223,7 +208,7 @@ const operations = reactive({
     connecting.value = true;
     // Get Available Operations
     response = await axios.get("/api/operations?ignore=" + (filter.ignore ? "yes" : "no") + "&unfinished=" + (filter.unfinished ? "yes" : "no"), {
-      headers: {Authorization: "Bearer " + cookies.get("access_token")},
+      headers: { Authorization: "Bearer " + cookies.get("access_token") },
     });
     if (response.status != 200) {
       message.error("无法获取现有行动");
@@ -233,7 +218,7 @@ const operations = reactive({
 
     // Get Operation Progress
     response = await axios.get("/api/operations/progress?simple=yes", {
-      headers: {Authorization: "Bearer " + cookies.get("access_token")},
+      headers: { Authorization: "Bearer " + cookies.get("access_token") },
     });
     if (response.status != 200) {
       message.error("无法获取现有行动进度");
@@ -243,7 +228,7 @@ const operations = reactive({
 
     // Get Operation Records
     response = await axios.get("/api/operations/logs?ignore=" + (!filter.working ? "yes" : "no"), {
-      headers: {Authorization: "Bearer " + cookies.get("access_token")},
+      headers: { Authorization: "Bearer " + cookies.get("access_token") },
     });
     if (response.status != 200) {
       message.error("无法获取行动记录");
@@ -255,12 +240,13 @@ const operations = reactive({
 });
 
 watch(
-    store.profile,
-    async () => {
-      await operations.fetch();
-    },
-    {immediate: true, deep: true}
+  store.profile,
+  async () => {
+    await operations.fetch();
+  },
+  { immediate: true, deep: true }
 );
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
